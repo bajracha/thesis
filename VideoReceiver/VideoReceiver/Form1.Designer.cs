bd385ac73041c41c9b0d2e7ab1a6b2f1a7bc53ab
nishikar <niishiikar@gmail.com>
2012-10-10 10:42:34 -0700

    video receiver initial commit.

---
+++ VideoReceiver/VideoReceiver/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VideoReceiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pbFrame = new System.Windows.Forms.PictureBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pbPlayback = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayback)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pbFrame
+            // 
+            this.pbFrame.Location = new System.Drawing.Point(12, 60);
+            this.pbFrame.Name = "pbFrame";
+            this.pbFrame.Size = new System.Drawing.Size(304, 217);
+            this.pbFrame.TabIndex = 1;
+            this.pbFrame.TabStop = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(103, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(346, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // pbPlayback
+            // 
+            this.pbPlayback.Location = new System.Drawing.Point(346, 60);
+            this.pbPlayback.Name = "pbPlayback";
+            this.pbPlayback.Size = new System.Drawing.Size(304, 217);
+            this.pbPlayback.TabIndex = 4;
+            this.pbPlayback.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 320);
+            this.Controls.Add(this.pbPlayback);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.pbFrame);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Video Receive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayback)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pbFrame;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.PictureBox pbPlayback;
+    }
+}
+
---
+++ VideoReceiver/VideoReceiver/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace VideoReceiver
+{
+    public partial class Form1 : Form
+    {
+        const int listenPort = 11000;
+        UdpClient listener;
+        IPAddress receiveAddress;
+        IPEndPoint groupEP;        
+        byte[] receiveByteArray;
+        List<byte[]> saveByteArray;
+        List<int> saveFrameSize;
+        MemoryStream imgStream;
+        Bitmap bmpImage;
+        int fileCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listener = new UdpClient(listenPort);
+            //receiveAddress = IPAddress.Parse("127.0.0.1");
+            receiveAddress = IPAddress.Parse("192.168.0.101");
+            groupEP = new IPEndPoint(receiveAddress, listenPort);            
+            imgStream = null;
+            bmpImage = null;
+            saveByteArray = new List<byte[]>();
+            saveFrameSize = new List<int>();
+            fileCount = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                {
+                    receiveByteArray = listener.Receive(ref groupEP);
+                    //Console.WriteLine(receiveByteArray.Length.ToString());
+
+                    //code to save data
+                    saveByteArray.Add(receiveByteArray);
+                    saveFrameSize.Add(receiveByteArray.Length);
+                    //saveFrameSize.Add(BitConverter.GetBytes(receiveByteArray.Length));
+                    //Console.WriteLine(saveFrameSize[0].ToString());
+                    if (saveByteArray.Count >= 40)
+                    {
+                        fileCount++;
+                        String vidFilename = Directory.GetCurrentDirectory()+"Video" + fileCount.ToString() + ".dat";
+                        String sizeFilename = Directory.GetCurrentDirectory()+"Siz" + fileCount.ToString() + ".dat";
+                        FileStream vidFileStream = new FileStream(vidFilename, FileMode.Create, FileAccess.Write);
+                        StreamWriter sizeFileStream = new StreamWriter(sizeFilename);
+                        
+                        foreach( byte[] byteArrayElement in saveByteArray )                         
+                            vidFileStream.Write(byteArrayElement, 0, byteArrayElement.Length);
+
+                        
+                        foreach (int sizeArrayElement in saveFrameSize)
+                            sizeFileStream.WriteLine(sizeArrayElement);
+                       
+                           
+                        saveByteArray.Clear();
+                        saveFrameSize.Clear();
+                        vidFileStream.Close();
+                        sizeFileStream.Close();
+                    }
+
+                    //code to display live data
+                    imgStream = new MemoryStream(receiveByteArray);
+                    bmpImage = new Bitmap(imgStream);
+                    imgStream.Close();
+                    if (bmpImage != null)
+                    {
+                        pbFrame.Image = bmpImage;
+                    }
+                });
+            }
+            catch
+            {
+            }
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Application.ApplicationExit += new EventHandler(delegate(object sender1, EventArgs e1)
+            {
+                //System.Console.WriteLine("socket closed");
+                listener.Close();
+            });
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            /*
+            String vidFilename = "C:\\Users\\Mahesh\\Documents\\Vid1.dat";
+            String sizeFilename = "C:\\Users\\Mahesh\\Documents\\Siz1.dat";
+            FileStream vidFileStream = new FileStream(vidFilename, FileMode.Open, FileAccess.Read);
+            FileStream sizeFileStream = new FileStream(sizeFilename, FileMode.Open, FileAccess.Read);
+            StreamReader sizeReader = new StreamReader(sizeFilename);
+            String s = sizeReader.ReadLine();
+            int size = int.Parse(s);
+            byte[] frameData;
+            MemoryStream frameStream;
+            Bitmap frame;
+
+            try
+            {
+                Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                {
+                    if (s != null)
+                    {
+                        frameData = new byte[size];
+                        vidFileStream.Read(frameData, 0, size);
+                        frameStream = new MemoryStream(frameData);
+                        frame = new Bitmap(frameStream);
+                        frameStream.Close();
+                        pbPlayback.Image = frame;                       
+                        s = sizeReader.ReadLine();
+                        try
+                        {
+                            size = int.Parse(s);
+                        }
+                        catch { ;}
+                    }
+
+                });
+            }
+            catch { ;}
+            finally
+            {
+                //sizeReader.Close();
+            }
+            */
+
+            String vidFilename;
+            String sizeFilename;
+            FileStream vidFileStream;            
+            StreamReader sizeReader;
+            String s;
+            int size;
+            byte[] frameData;
+            MemoryStream frameStream;
+            Bitmap frame;
+            int fileCount = 0;
+
+            try
+            {
+                fileCount++;
+                vidFilename = Directory.GetCurrentDirectory()+"Video" + fileCount.ToString() + ".dat";
+                sizeFilename = Directory.GetCurrentDirectory()+"Siz" + fileCount.ToString() + ".dat";
+                vidFileStream = new FileStream(vidFilename, FileMode.Open, FileAccess.Read);
+                sizeReader = new StreamReader(sizeFilename);
+                s = sizeReader.ReadLine();
+                size = int.Parse(s);        
+
+                Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                {                        
+
+                    if (s != null)
+                    {
+                        frameData = new byte[size];
+                        vidFileStream.Read(frameData, 0, size);
+                        frameStream = new MemoryStream(frameData);
+                        frame = new Bitmap(frameStream);
+                        frameStream.Close();
+                        pbPlayback.Image = frame;
+                        s = sizeReader.ReadLine();
+                        try
+                        {
+                            size = int.Parse(s);
+                        }
+                        catch { ;}
+                    }
+                    else
+                    {
+                        vidFileStream.Close();
+                        sizeReader.Close();
+                        fileCount++;
+                        vidFilename = Directory.GetCurrentDirectory()+"Video" + fileCount.ToString() + ".dat";
+                        sizeFilename = Directory.GetCurrentDirectory()+"Siz" + fileCount.ToString() + ".dat";
+                        vidFileStream = new FileStream(vidFilename, FileMode.Open, FileAccess.Read);
+                        sizeReader = new StreamReader(sizeFilename);
+                        s = sizeReader.ReadLine();
+                        try
+                        {
+                            size = int.Parse(s);
+                        }
+                        catch { ;}
+                    }
+
+                });
+            }
+            catch { ;}           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+    }
+}
