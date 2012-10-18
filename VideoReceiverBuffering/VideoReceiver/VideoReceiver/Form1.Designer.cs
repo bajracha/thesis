670f27c393e377379b1cd5b263da2059d7d0f0e3
nishikar <niishiikar@gmail.com>
2012-10-17 21:57:10 -0700

    Video receiver with buffering.

---
+++ VideoReceiverBuffering/VideoReceiver/VideoReceiver/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace VideoReceiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pbFrame = new System.Windows.Forms.PictureBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progDisplay = new System.Windows.Forms.ProgressBar();
+            this.progBuffer = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pbFrame
+            // 
+            this.pbFrame.Location = new System.Drawing.Point(12, 60);
+            this.pbFrame.Name = "pbFrame";
+            this.pbFrame.Size = new System.Drawing.Size(320, 240);
+            this.pbFrame.TabIndex = 1;
+            this.pbFrame.TabStop = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(103, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progDisplay
+            // 
+            this.progDisplay.ForeColor = System.Drawing.Color.ForestGreen;
+            this.progDisplay.Location = new System.Drawing.Point(12, 306);
+            this.progDisplay.Name = "progDisplay";
+            this.progDisplay.Size = new System.Drawing.Size(319, 17);
+            this.progDisplay.TabIndex = 3;
+            // 
+            // progBuffer
+            // 
+            this.progBuffer.ForeColor = System.Drawing.Color.Gray;
+            this.progBuffer.Location = new System.Drawing.Point(12, 329);
+            this.progBuffer.Name = "progBuffer";
+            this.progBuffer.Size = new System.Drawing.Size(319, 17);
+            this.progBuffer.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 562);
+            this.Controls.Add(this.progBuffer);
+            this.Controls.Add(this.progDisplay);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.pbFrame);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Video Receive";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pbFrame;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progDisplay;
+        private System.Windows.Forms.ProgressBar progBuffer;
+    }
+}
+
---
+++ VideoReceiverBuffering/VideoReceiver/VideoReceiver/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Management;
+//using System.Reflection;
+//using System.Reflection.Emit;
+
+namespace VideoReceiver
+{
+    public partial class Form1 : Form
+    {
+        const int listenPort = 11000;
+        UdpClient listener;
+        IPAddress receiveAddress;
+        IPEndPoint groupEP;
+        byte[] receiveByteArray;
+        MemoryStream imgStream;
+        Bitmap bmpImage;
+        Thread buffering;
+        Thread displaying;
+        CircularBuffer frameBuffer;
+        //DynamicMethod assemblyMethod;
+        //ILGenerator assemblyGen;
+        //System.Threading.Timer delay;
+        //System.Threading.TimerCallback delayCallBack;
+
+        int d; //for storing difference betn head and tail of buffer
+
+        DateTime dt1, dt2;
+        TimeSpan ts;
+
+        //ApplicationContext a;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listener = new UdpClient(listenPort);
+            receiveAddress = IPAddress.Parse("127.0.0.1");
+            //receiveAddress = IPAddress.Parse("131.216.16.125");
+            groupEP = new IPEndPoint(receiveAddress, listenPort);
+            receiveByteArray = null;
+            imgStream = null;
+            bmpImage = null;
+            frameBuffer = new CircularBuffer();
+
+            displaying = new Thread(this.Display);
+            buffering = new Thread(this.FillBuffer);
+            //delayCallBack = new TimerCallback(GenerateDelay);
+
+            //assemblyMethod = new DynamicMethod("NoOperation", null, Type.EmptyTypes);
+            //assemblyGen = assemblyMethod.GetILGenerator();
+            this.resetProgress();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            
+            
+
+            //Console.WriteLine(this.CPUSpeed());            
+            /*
+            DateTime dt1 = DateTime.Now;            
+            for (int i = 0; i < 1000000; i++) { }
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = dt2 - dt1;
+            Console.WriteLine(ts.Milliseconds);
+            */
+        }
+
+        private void resetProgress()
+        {
+            progBuffer.Minimum = 1;
+            progBuffer.Maximum = 2000;
+            progBuffer.Value = 1;
+            progBuffer.Step = 1;
+
+            progDisplay.Minimum = 1;
+            progDisplay.Maximum = 2000;
+            progDisplay.Value = 1;
+            progDisplay.Step = 1;
+        }
+
+        /*private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                {
+                    receiveByteArray = listener.Receive(ref groupEP);
+                    imgStream = new MemoryStream(receiveByteArray);
+                    bmpImage = new Bitmap(imgStream);
+                    imgStream.Close();
+                    if (bmpImage != null)
+                    {
+                        pbFrame.Image = bmpImage;
+                    }
+                });
+            }
+            catch
+            {
+            }
+            
+        }*/
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //start = true;
+            int d = 0;
+            try
+            {
+                 
+                if (buffering.ThreadState.ToString().Equals("Unstarted"))
+                    buffering.Start();
+                if (displaying.ThreadState.ToString().Equals("Unstarted"))
+                    displaying.Start();
+                /*
+                //while (true)
+                //{
+                
+                //Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                //{
+
+                    d = frameBuffer.diff();
+                    //Console.WriteLine(d);
+                    if (d < 10)
+                    {
+                        //Console.WriteLine("Buffering..");
+                        displaying.Suspend();
+                    }
+                    else
+                    {
+                        if (displaying.ThreadState.ToString().Equals("Suspended"))
+                            displaying.Resume();
+                    }
+
+                //});
+               // }*/
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void Display()
+        {
+            //int count = 100;
+
+            Console.WriteLine("Display...");
+            while (true)
+            {
+                //Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                //{
+                if (frameBuffer.size() >= 50)
+                {
+                    try
+                    {
+                        //if (count > 0)
+                        //{
+                        imgStream = new MemoryStream(frameBuffer.read());
+                        bmpImage = new Bitmap(imgStream);
+                        imgStream.Close();
+                        if (bmpImage != null)
+                        {
+                            pbFrame.Image = bmpImage;                            
+                            progDisplay.PerformStep();                            
+                        }
+
+                        //}
+                        //count--;
+
+                        dt1 = DateTime.Now;
+                        //delay = new System.Threading.Timer(delayCallBack, null, 0, 5000);
+                        Thread.Sleep(25);
+                        dt2 = DateTime.Now;
+                        ts = dt2 - dt1;
+                        Console.WriteLine(ts.Milliseconds);
+                        //delay.Dispose();
+                            
+
+
+                        //Thread.Sleep(15);
+                        /*dt1 = DateTime.Now;
+                        for (int i = 0; i < 300000; i++)
+                        {
+                            assemblyGen.Emit(OpCodes.Nop);
+                        }
+                        dt2 = DateTime.Now;
+                        ts = dt2 - dt1;*/
+                        //Console.WriteLine(this.CPUSpeed());
+                        //Console.WriteLine(ts.Milliseconds);
+
+
+                            
+                    }
+                    catch(Exception ex) 
+                    {
+                        Console.WriteLine("Display " + ex.Message);
+                        //Console.Clear();
+                    }
+                }
+            }
+           // });            
+
+        }
+
+        /*private void GenerateDelay(Object o)
+        {
+            ;
+        }*/
+
+        private void FillBuffer()
+        {
+            Console.WriteLine("filling...");            
+            while (true)
+            {
+                // Application.Idle += new EventHandler(delegate(object sender1, EventArgs e1)
+                // {
+                try
+                {
+                    receiveByteArray = listener.Receive(ref groupEP);
+                    frameBuffer.enqueue(ref receiveByteArray);
+
+                    d = frameBuffer.diff();
+                    if (d < 10)
+                    {
+                        displaying.Suspend();
+                    }
+                    else
+                    {
+                        if (displaying.ThreadState.ToString().Equals("Suspended"))
+                            displaying.Resume();
+                    }
+                    
+                    if (progBuffer.Value >= progBuffer.Maximum)
+                        this.resetProgress();
+                    else
+                        progBuffer.PerformStep();                   
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Buffer " + ex.Message);                    
+                }
+            }
+
+            //});
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //start = false;
+                buffering.Abort();
+                //while (buffering.IsAlive) { }
+                if (displaying.ThreadState.ToString().Equals("Suspended"))
+                    displaying.Resume();
+                displaying.Abort();
+                //while (displaying.IsAlive) { }
+                listener.Close();               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Application.Exit();
+            }
+            
+            /*Application.ApplicationExit += new EventHandler(delegate(object sender1, EventArgs e1)
+            {
+                //System.Console.WriteLine("socket closed");
+                
+                try
+                {
+                    buffering.Abort();
+                    displaying.Abort();
+                    listener.Close();                    
+                }
+                catch { }
+            });*/
+        }
+
+        private uint CPUSpeed()
+        {
+            ManagementObject Mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'");
+            uint sp = (uint)(Mo["CurrentClockSpeed"]);
+            Mo.Dispose();
+            return sp;
+        }
+
+        
+    }
+}
